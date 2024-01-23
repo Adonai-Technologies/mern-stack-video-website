6e9233b3255003c23cdf1cfc478eb6834333f621
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2024-01-23 19:43:54 +0100

    Added video routes

--- controllers/Video.js
+++ controllers/Video.js
@@ -1,26 +1,59 @@
 module.exports = {
-    addVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-    
-    updateVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-    
-    deleteVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-    
-    getVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-    
-    likeVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-    
-    
-    dislikeVideo: async (req,res,next)=>{
-        res.render('video.ejs')
-    },
-}
+  addVideo: async (req, res, next) => {
+    const newVideo = new Video({ userId: req.user._d, ...req.body });
+    try {
+      const saveVideo = await newVideo.save();
+      res.status(200).json(saveVideo);
+    } catch (error) {
+      next(err);
+    }
+  },
+
+  updateVideo: async (req, res, next) => {
+    try {
+      const video = await Video.findById(req.params._d);
+      if (!video) return next(Error(404, "Video not found"));
+      if (req.user._d === video.userId) {
+        const updatedVideo = await Video.findByIdAndUpdate(
+          req.params._d,
+          {
+            $set: req.body
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedVideo)
+      }
+    } catch (error) {
+      next(err);
+    }
+  },
+
+  deleteVideo: async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+      next(err);
+    }
+  },
+
+  getVideo: async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(err);
+    }
+  },
+
+  likeVideo: async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(err);
+    }
+  },
+
+  dislikeVideo: async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(err);
+    }
+  }
+};
